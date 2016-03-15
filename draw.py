e32964198fb9f4a6f7006a97f61580ad62c0d948
daltonwu <dalton.z.wu@gmail.com>
2016-03-15 11:32:03 -0400

    Update

--- draw.py
+++ draw.py
@@ -2,36 +2,34 @@ from display import *
 from matrix import *
 import math
 
-def add_circle( points, cx, cy, cz, r, step ):
+def add_circle(points, cx, cy, cz, r, step):
     pass
 
-def add_curve( points, x0, y0, x1, y1, x2, y2, x3, y3, step, curve_type ):
+def add_curve(points, x0, y0, x1, y1, x2, y2, x3, y3, step, curve_type):
     pass
 
-def draw_lines( matrix, screen, color ):
-    if len( matrix ) < 2:
+def draw_lines(matrix, screen, color):
+    if len(matrix) < 2:
         print "Need at least 2 points to draw a line"
-        
-    p = 0
-    while p < len( matrix ) - 1:
-        draw_line( screen, matrix[p][0], matrix[p][1],
-                   matrix[p+1][0], matrix[p+1][1], color )
-        p+= 2
+    
+    for i in range(0, len(matrix), 2):
+        draw_line(screen, matrix[i][0], matrix[i][1],
+                  matrix[p+1][0], matrix[p+1][1], color)
 
-def add_edge( matrix, x0, y0, z0, x1, y1, z1 ):
-    add_point( matrix, x0, y0, z0 )
-    add_point( matrix, x1, y1, z1 )
+def add_edge(matrix, x0, y0, z0, x1, y1, z1):
+    add_point(matrix, x0, y0, z0)
+    add_point(matrix, x1, y1, z1)
 
-def add_point( matrix, x, y, z=0 ):
-    matrix.append( [x, y, z, 1] )
+def add_point(matrix, x, y, z=0):
+    matrix.append([x, y, z, 1])
 
-
-def draw_line( screen, x0, y0, x1, y1, color ):
+def draw_line(screen, x0, y0, x1, y1, color):
     dx = x1 - x0
     dy = y1 - y0
+    
     if dx + dy < 0:
-        dx = 0 - dx
-        dy = 0 - dy
+        dx = -dx
+        dy = -dy
         tmp = x0
         x0 = x1
         x1 = tmp
@@ -43,12 +41,12 @@ def draw_line( screen, x0, y0, x1, y1, color ):
         y = y0
         while y <= y1:
             plot(screen, color,  x0, y)
-            y = y + 1
+            y += 1
     elif dy == 0:
         x = x0
         while x <= x1:
             plot(screen, color, x, y0)
-            x = x + 1
+            x += 1
     elif dy < 0:
         d = 0
         x = x0
@@ -56,10 +54,10 @@ def draw_line( screen, x0, y0, x1, y1, color ):
         while x <= x1:
             plot(screen, color, x, y)
             if d > 0:
-                y = y - 1
-                d = d - dx
-            x = x + 1
-            d = d - dy
+                y -= 1
+                d -= dx
+            x += 1
+            d -= dy
     elif dx < 0:
         d = 0
         x = x0
@@ -67,10 +65,10 @@ def draw_line( screen, x0, y0, x1, y1, color ):
         while y <= y1:
             plot(screen, color, x, y)
             if d > 0:
-                x = x - 1
-                d = d - dy
-            y = y + 1
-            d = d - dx
+                x -= 1
+                d -= dy
+            y += 1
+            d -= dx
     elif dx > dy:
         d = 0
         x = x0
@@ -78,10 +76,10 @@ def draw_line( screen, x0, y0, x1, y1, color ):
         while x <= x1:
             plot(screen, color, x, y)
             if d > 0:
-                y = y + 1
-                d = d - dx
-            x = x + 1
-            d = d + dy
+                y += 1
+                d -= dx
+            x += 1
+            d += dy
     else:
         d = 0
         x = x0
@@ -89,8 +87,7 @@ def draw_line( screen, x0, y0, x1, y1, color ):
         while y <= y1:
             plot(screen, color, x, y)
             if d > 0:
-                x = x + 1
-                d = d - dy
-            y = y + 1
-            d = d + dx
-
+                x += 1
+                d -= dy
+            y += 1
+            d += dx
